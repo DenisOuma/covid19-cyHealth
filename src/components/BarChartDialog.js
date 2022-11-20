b976d28ab6bf7d8dd417b7b400ed616b6ab258f5
DenisOuma <dennisouma38@gmail.com>
2022-11-20 12:45:15 +0300

    Implimenting the bar chart table on the project

--- src/components/BarChartDialog.js
+++ src/components/BarChartDialog.js
@@ -8,6 +8,15 @@ import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
 import Slide from "@mui/material/Slide";
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import {
+	BarChart,
+	Bar,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+} from "recharts";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
 	return <Slide direction="up" ref={ref} {...props} />;
@@ -23,6 +32,17 @@ export default function BarChartDialog() {
 	const handleClose = () => {
 		setOpen(false);
 	};
+	const data = [
+		{ name: "A", x: 12, y: 23, z: 122 },
+		{ name: "B", x: 22, y: 3, z: 73 },
+		{ name: "C", x: 13, y: 15, z: 32 },
+		{ name: "D", x: 44, y: 35, z: 23 },
+		{ name: "E", x: 35, y: 45, z: 20 },
+		{ name: "F", x: 62, y: 25, z: 29 },
+		{ name: "G", x: 37, y: 17, z: 61 },
+		{ name: "H", x: 28, y: 32, z: 45 },
+		{ name: "I", x: 19, y: 43, z: 93 },
+	];
 
 	return (
 		<div>
@@ -72,6 +92,18 @@ export default function BarChartDialog() {
 					</Toolbar>
 				</AppBar>
 				<h1>Country Bar Graph</h1>
+				<div className="inner-container">
+					<BarChart width={800} height={800} data={data}>
+						<CartesianGrid />
+						<XAxis dataKey="name" />
+						<YAxis />
+						<Tooltip />
+						<Legend />
+						<Bar dataKey="x" stackId="a" fill="#8884d8" />
+						<Bar dataKey="y" stackId="a" fill="#82ca9d" />
+						<Bar dataKey="y" stackId="a" fill="red" />
+					</BarChart>
+				</div>
 			</Dialog>
 		</div>
 	);
